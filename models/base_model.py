50234ddba9d2d343d3f188b8cd4c03abdafe0ad7
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-06-26 19:47:44 -0500

    add BaseModel complete Tassk 2

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,55 @@
 #!/usr/bin/python3
-""""""
+"""base_model module"""
 import uuid
 from datetime import *
 import models
 
 
 class BaseModel():
-    """"""
+    """
+    Defines all common attributes/methods for other class
+    """
 
-    def __init__(self):
-        """"""
-        #dt_string = '2017-06-14T22:31:03.285259'
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
-        self.id = str(uuid.uuid4())
+    def __init__(self, *args, **kwargs):
+        """Constructor
+        Arguments:
+            *args: won’t be used
+            **kwargs: is not empty"""
+        fd = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    self.created_at = datetime.strptime(v, fd)
+                elif k == "updated_at":
+                    self.updated_at = datetime.strptime(v, fd)
+                elif k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, v)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
 
-    def save(self):
-        """"""
-        self.update_at = datetime.now()
-
-    def to_dict(self):
-        """"""
-        dic = self.__dict__.copy()
-        dic['__class__'] = BaseModel.__name__
-        dic['create_at'] = self.create_at.isoformat()
-        dic['update_at'] = self.update_at.isoformat()
-        return(dic)
 
     def __str__(self):
-        str="[<{}>] (<{}>) <{}>"
+        """Return a String that contains the class name, \
+        the ID and dictionary
+        """
+        str="[{}] ({}) {}"
         return (str.format(BaseModel.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all \
+        keys/values of __dict__ of the instance"""
+
+        dic = dict(self.__dict__)
+        dic['__class__'] = BaseModel.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+
+        return (dic)

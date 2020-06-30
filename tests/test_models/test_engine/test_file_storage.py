d77f1058c961b1f3992dae4b61a4a00773d90cb8
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-06-30 13:12:06 -0500

    add Test Cases docstrings

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""""""
+import unittest
+import os
+import contextlib
+
+# class
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_docstrings(self):
+        """"""
+        self.assertTrue(FileStorage.all.__doc__)
+        self.assertTrue(hasattr(FileStorage, 'all'))
+        self.assertTrue(FileStorage.new.__doc__)
+        self.assertTrue(hasattr(FileStorage, 'new'))
+        self.assertTrue(FileStorage.save.__doc__)
+        self.assertTrue(hasattr(FileStorage, 'save'))
+        self.assertTrue(FileStorage.reload.__doc__)
+        self.assertTrue(hasattr(FileStorage, 'reload'))
+
+
+if __name__ == '__main__':
+    unittest.main()
